2acc4d9d1a8f31e5b8b2acdec9b4f2e14c268982
Gerrit Doornenbal <g.doornenbal@gmail.com>
2017-03-01 15:02:15 +0100

    Big update, two new commands.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # monitoring-plugins
-Here i put some created or modified monitoring plugins voor nagios/opsview/icinga
+Here i put some created or modified monitoring plugins voor nagios/opsview/icinga.
 
 #check_snmp_mem
 
@@ -7,6 +7,23 @@ An old but usefull check for cisco, procurve and linux.  As we had also use hpux
 
 #check_snmp_storage
 
-This one was already for linux and windows. added also hpux.  Same write had another plugin for that, which ws almost the same. so i put them together.
+This one was already for linux and windows. added also hpux.  Same writer had another plugin for that, which was almost the same. so i put them together.
 
+#check_smb_share
 
+This is an old one, which i gave a huge update, so the following checks are now possible:
+ * existence of a share.
+ * existence of a file or directory on a share. (optional inside a directory.)
+ * check that a file or wildcard does NOT exist on a share.
+ * check the maximum age of a file in seconds.
+ * using a passwordfile for credentials
+ 
+#check_mysql_query.pl
+
+This is also an old one created once by Michal Sviba, now with a huge update. 
+in short the following:
+ * code cleanup
+ * added verbose, performance output options.
+ * modified default warning/critical behavior. (no defaults anymore)
+ * added option to give complete QUERY instead of parts.
+ * added options to use an sql file as query
---
+++ check_mysql_query.pl
@@ -0,0 +1,182 @@
+#!/usr/bin/perl
+#####################################################################
+#
+# check_mysql_query.pl
+#  jan 2017 Gerrit Doornenbal
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# feb 2017 by Gerrit Doornenbal
+#   Huge rewrite, several debugging and other options added.
+#   added SQL option, with file and direct SQL.
+#
+# This script is based on:
+#   check_mysql_count.pl version 0.02
+#   2008-2011 Michal Sviba
+#   contact the author directly for more information at:
+#   michal at myserver.cz
+#
+#####################################################################
+#
+# Check output of a sql query.  The output header is used as name for the output.
+# This plugin requires that mysql is installed on the system.
+#
+
+use strict;
+use Getopt::Long;
+
+#sub print_usage;
+
+my $TIMEOUT = 15;
+my $MYSQL = "mysql";
+my %ERRORS = ('UNKNOWN' , '-1',
+              'OK' , '0',
+              'WARNING', '1',
+              'CRITICAL', '2');
+my $state = "OK";
+my $count = 0;
+my $countname = "";
+my $status;
+
+my $host="localhost";
+my $port=3306;
+my $user;
+my $pass;
+my ($DB, $TABLE, $COND, $QUERY, $SQL);
+my ($warn, $crit);
+my ($perf, $debug);
+
+#Start 
+check_options();
+
+#Create the correct SQL query
+if (defined($QUERY)) {
+	#test for sql file or direct query
+	if (index($QUERY, ".sql") != -1) {
+		$SQL = "source $QUERY;";	
+		if (defined($debug)) { 
+			print "SQL script file:\n"; 
+			open FILE, "$QUERY" or die "Could not open $QUERY";
+				while(<FILE>) {
+				print $_;
+				}
+			close FILE;
+			print "\n"; 
+			}
+	} else { 
+		$SQL = "$QUERY;";	
+	}
+} else { 
+	$COND =~ s/'/\\'/g;
+	if ($COND ne "") {$COND = "WHERE $COND";}
+	$SQL = "SELECT COUNT(1) \"rows\" FROM $TABLE $COND;";
+}
+
+if (defined($debug)) { print ("SQL query: $SQL \n");}
+
+#Start the query
+open (OUTPUT, "$MYSQL -B -h $host -u $user --password=\"$pass\" $DB -e '$SQL' 2>&1 |");
+#And read the SQL output.
+while (<OUTPUT>) {
+	if (/failed/||/ERROR/) { 
+		#set state and statustext when error occurs
+		$state="CRITICAL"; 
+		$status="$state: $_"; 
+		last; 
+		}
+	chomp;
+	if ($countname eq "") { #first field = name, second = value
+		$countname = $_
+		} else { 
+		$count = $_;
+		}
+}
+if (defined($debug)) { print "name=$countname count=$count\n"; }
+
+#Check for warn/crit criteria
+if (defined($warn) && defined($crit) ) {
+if ($count >= $warn) { $state = "WARNING"; }
+if ($count >= $crit) { $state = "CRITICAL"; }
+}
+if ($count =~ m/\D/) { $state = "UNKNOWN"; }
+#Create correct output, including perfdata.
+if ( not defined($status) ){ 
+	if ( defined($TABLE) ){ 
+		$TABLE="in table $TABLE";
+	} else { 
+		$TABLE="found";
+	}
+	$status = "$state: $count $countname $TABLE"; 
+	if ( defined($perf) ) {
+		$status = "$status|'$countname'=$count;$warn;$crit;0;"
+	}
+}
+
+print "$status\n";
+exit $ERRORS{$state};
+
+sub check_options () {
+	if ( $ARGV[0] eq '')
+		{  
+		print "Required arguments not given!\n\n";
+		print_usage() 
+		}
+
+	my $o_help;
+	my $o_debug;
+
+	Getopt::Long::Configure ("bundling");
+	GetOptions(
+		'h|help'	=> \$o_help,
+		'v|verbose'	=> \$debug,
+		'H|hostname:s'	=> \$host,
+		'P|port:i'	=> \$port,
+		'u|user:s'	=> \$user,
+		'p|pass:s' => \$pass,
+		'D|database:s' => \$DB,
+		'T|table:s' => \$TABLE,
+		'C|cond:s' => \$COND,
+		'Q|query:s' => \$QUERY,
+		'w|warn:i' => \$warn,
+		'c|crit:i'	=> \$crit,
+		'f|perf'	=> \$perf,
+	);
+
+	print_usage() if (defined($o_help));
+	$debug = 1 if (defined($o_debug));
+}
+
+sub print_usage {
+	print "MySQL plugin for Nagios, version 0.2; 2008-2011 Michal Sviba; 2017 Gerrit Doornenbal\n\n";
+	print "Script for running a SQL script @ MySQL.  The result of the SQL script should contain two lines with each one value:\n";
+	print "   line 1: the name for the value, line 2: the value.\n\n";
+	print "Usage: check_mysql_query.pl -H <host> -u <user> -p <pass> -D <db> -T <table> -C <cond> -Q <SQL query or file> [-w <warn> -c <crit> -f -v] \n";
+	print "   -u username to login to mysql at <host>\n";
+	print "   -p password with SELECT privilege to use for <user> at <host>\n";
+	print "   -D DB where table is placed \n";
+	print "    -T table in DB\n";
+	print "    -C conditionals in where section. (without 'where')\n";
+	print "   or\n";
+	print "    -Q Instead of table and conditionals you can also give a SQL query or a .sql filename with the query.\n";
+	print "       This gives you the opportunity to use more complicated scripts.\n";
+	print "   -w number of rows to warning state.\n";
+	print "   -c number of rows to critical state.\n";
+	print "   -f Show performance output.\n";
+	print "   -v Verbose output, nice for testing your scripts.\n";
+	print "Examples:\n";
+	print "   check_mysql_query.pl -H hostname -u user -p yourpass -D mysql -T user -w 10 -c 20\n";
+	print "      this one counts the number of rows in the mysql user table, and gives warning >10 rows, critical >20 rows.\n";
+	print "   check_mysql_query.pl -H hostname -u user -p yourpass -D mysql -Q 'SELECT COUNT(1) \"rows\" FROM user' -f\n";
+	print "      counts the number of rows in the mysql user table, gives also performance output.\n";
+	print "   check_mysql_query.pl -H hostname -u user -p yourpass -D mysql -Q script.sql -f\n";
+	print "      Execute the script inside script.sql on database mysql, with performance output.\n";
+	exit $ERRORS{"UNKNOWN"};
+}
